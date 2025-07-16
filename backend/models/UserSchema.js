cfd5c4a32b13ec1cce9c00dbbcb3461b256a7770
Anandvivek-iiti <anand1234.vivek@gmail.com>
2025-07-16 09:43:26 +0530

    Clubs  added

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
       minlength: [6, "Password must be at least 6 characters long"],
       maxlenght: [10, "Password must be at most 10 characters long"],
     },
-   user_photo: {
+   userphoto: {
     type:String,
     required:false
    }
