aa8a274676ba424cd2aa7dec918fa197e963d04e
Sakshya2504 <singhkaserasakshya2504@gmail.com>
2025-07-17 11:05:11 +0530

    ER-12

--- Backened/models/Announce.js
+++ Backened/models/Announce.js
@@ -1,10 +1,28 @@
 import mongoose from "mongoose";
 
-const Announce = new mongoose.Schema({
-   clubname: { type: String, required: true, minlength: [1, 'Club name must be at least 1 characters long'], maxlength: [50, 'Club name must be at most 50 characters long'] },
-   heading: { type: String, required: true, minlength: [1, 'Heading must be at least 1 characters long'], maxlength: [100, 'Heading must be at most 100 characters long'] },
-   info: { type: String, required: true, minlength: [1, 'Info must be at least 1 characters long'], maxlength: [500, 'Info must be at most 500 characters long'] },
-   announcelogo:{type:String,required:[true,"Please Provide the Announcement Logo"]}
+const AnnounceSchema = new mongoose.Schema({
+   clubname: {
+      type: String,
+      required: [true, 'Club name must be provided'],
+      minlength: [1, 'Club name must be at least 1 character long'],
+      maxlength: [50, 'Club name must be at most 50 characters long']
+   },
+   heading: {
+      type: String,
+      required: [true, 'Heading must be provided'],
+      minlength: [1, 'Heading must be at least 1 character long'],
+      maxlength: [100, 'Heading must be at most 100 characters long']
+   },
+   info: {
+      type: String,
+      required: [true, 'Info must be provided'],
+      minlength: [1, 'Info must be at least 1 character long'],
+      maxlength: [500, 'Info must be at most 500 characters long']
+   },
+   announcelogo: {
+      type: Object,
+      required: [true, 'Announcement logo must be provided']
+   }
 }, { timestamps: true });
 
-export const Announce_ = mongoose.model('Announce', Announce);
+export const Announce_ = mongoose.model('Announce', AnnounceSchema);
